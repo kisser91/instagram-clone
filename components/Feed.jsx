d5f11a8d2bc50f56672c39456cd4c16441a67a0b
kisser91 <nahuelkisser91@gmail.com>
2022-02-07 19:48:09 -0300

    fix ui grid

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,20 +7,24 @@ import {useSession} from "next-auth/react";
 function Feed() {
   const {data: session} = useSession();
   return (
-      <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-7xl mx-auto ${!session && " !grid-cols-1 !max-w-3xl"}`}>
-        <section className={`col-span-1 md:col-span-2  ${session && " xl:col-span-2"}` }>
+
+      <main 
+        className={`grid grid-cols-1  md:max-w-3xl  xl:max-w-7xl  
+        md:grid-cols-2 xl:grid-cols-3         
+        mx-auto`}>
+        <section className={`col-span-1 md:col-span-2 xl:col-span-3 ${session && "md:col-span-2 xl:col-span-2"}` }>
                 <Stories />
                 <Posts />
         </section>
         {session && (
-        <section className="hidden xl:inline-grid md:col-span-1">
+          <section className={`hidden xl:inline-grid md:col-span-1`}>
           <div className="fixed ">
                 <MiniProfile />
                 <Suggestions />
           </div>
         </section>
 
-        )}
+)}
       </main>
   )
 }
